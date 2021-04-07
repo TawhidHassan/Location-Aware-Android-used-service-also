e668137cd42c790cda740be2db9e45c3b3ef9021
sifat hassan <sifat007.sh7@gmail.com>
2021-04-07 11:38:23 +0600

    design done

--- app/src/main/java/com/example/locationawareandroiduserservice/MainActivity.java
+++ app/src/main/java/com/example/locationawareandroiduserservice/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.locationawareandroiduserservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonGetLocation, buttonStopLocation;
+    TextView textViewLatitude, textViewLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonGetLocation = (Button)findViewById(R.id.buttonGetLocation);
+        buttonStopLocation = (Button)findViewById(R.id.buttonStopLocation);
+
+        textViewLatitude = (TextView)findViewById(R.id.textViewLatitude);
+        textViewLongitude = (TextView)findViewById(R.id.textViewLongitude);
+
+
+        buttonGetLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        buttonStopLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               
+            }
+        });
+
     }
 }
